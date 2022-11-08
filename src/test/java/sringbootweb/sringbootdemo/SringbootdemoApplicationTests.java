1e407b04a37773f980bb1c27d1dd3d8e04564351
bharathmanohar96 <bharatcv13@gmail.com>
2022-11-08 15:56:22 +0530

    adding files

---
+++ src/test/java/sringbootweb/sringbootdemo/SringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sringbootweb.sringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
